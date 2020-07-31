b30c18027ab8b2ff9dd019cd1d86fce734306f76
Tyson Lackey <lackeyt90@gmail.com>
2020-07-31 11:17:34 -0700

    update clients create view

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace HairSalon.Models
 {
   public class Client
   {
     public int ClientId { get; set; }
     public int StylistId { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string Name { get; set; }
+    public int PhoneNumber { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
 }
