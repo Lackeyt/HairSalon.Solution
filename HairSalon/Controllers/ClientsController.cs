b949084fddbcd33b2750ae3a0b907af0faa1b4bb
Tyson Lackey <lackeyt90@gmail.com>
2020-07-31 14:41:10 -0700

    move all navbar links from the bottom to the jumbotron

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -45,6 +45,7 @@ namespace HairSalon.Controllers
     public ActionResult Edit (int id)
     {
       Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(thisClient);
     }
 
